ff9ddc505ae4077aac7ec2a164fb5aee4d37327f
Alexander Hritsun <ahritsun@opengov.com>
2020-08-26 14:39:23 +0300

    Add cucumber integration
    -Rabish files removed
    -Add the cucumber and nightwatch api packages
    -Add the cucumber.conf
    -Create the folder structure

--- tests/firsttest.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'My first testcase'(browser) {
-        browser
-            .url("https://news.ycombinator.com")
-            .waitForElementVisible('.hnname')
-            .assert.containsText(".hnname", "Hacker News");
-    }
-}
